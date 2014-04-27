ea0c3196dc7b561cb4e6c25349ac9e6771f0b907
Tyler Morgan <digismack@gmail.com>
2014-04-27 22:38:08 +0000

    ui interaction tweaks

--- static/js/radio.js
+++ static/js/radio.js
@@ -1,6 +1,6 @@
 function updatePanel(data){
-    $('#channel-setting').text( data['channel'] );
-    $('#volume-setting').text( data['volume'] );
+    $('#channel-setting').text(data['channel']);
+    $('#volume-setting').text(data['volume']);
 }
 
 $(document).ready(function(){
@@ -9,53 +9,88 @@ $(document).ready(function(){
         type : 'GET',
         url : '/info',
         dataType : 'json',
+        cache : false,
     });
 
-    initial_info.done(function( data ){
-        updatePanel( data );
+    initial_info.done(function(data){
+        updatePanel(data);
     });
     
     $('#volume-up').on('click', function(){
+        $button = $(this);
+        $button.attr('disabled','disabled');
         volume_up = $.ajax({
             type : 'GET',
             url : '/volume_up',
             dataType : 'json',
+            cache : false,
+            beforeSend: function(){
+                $('#volume-setting').text('-').css('color','#666');
+            }
         });
-        volume_up.done(function( data ){
-            updatePanel( data );
+        volume_up.done(function(data){
+            updatePanel(data);
+            $button.removeAttr('disabled');
+            $('#volume-setting').css('color','#fff');
         });
     });
+
     $('#volume-down').on('click', function(){
+        $button = $(this);
+        $button.attr('disabled','disabled');
         volume_down = $.ajax({
             type : 'GET',
             url : '/volume_down',
             dataType : 'json',
+            cache : false,
+            beforeSend: function(){
+                $('#volume-setting').text('-').css('color','#666');
+            }
         });
-        volume_down.done(function( data ){
-            updatePanel( data );
+        volume_down.done(function(data){
+            updatePanel(data);
+            $('#volume-setting').css('color','#fff');
+            $button.removeAttr('disabled');
         });
-
     });
+
     $('#seek-right').on('click', function(){
+        $button = $(this);
+        $button.attr('disabled','disabled');
         seek_right = $.ajax({
             type : 'GET',
             url : '/seek_right',
             dataType : 'json',
+            cache : false,
+            beforeSend: function(){
+                $('#channel-setting').text('-').css('color','#666');
+            }
         });
-        seek_right.done(function( data ){
-            updatePanel( data );
+        seek_right.done(function(data){
+            updatePanel(data);
+            $('#channel-setting').css('color','#fff');
+            $button.removeAttr('disabled');
         });
-
     });
+
     $('#seek-left').on('click', function(){
+        $button = $(this);
+        $button.attr('disabled','disabled');
+        $('#channel-setting').css('color','#666');
         seek_left = $.ajax({
             type : 'GET',
             url : '/seek_left',
             dataType : 'json',
+            cache : false,
+            beforeSend: function(){
+                $('#channel-setting').text('-').css('color','#666');
+            }
         });
-        seek_left.done(function( data ){
-            updatePanel( data );
+        seek_left.done(function(data){
+            updatePanel(data);
+            $('#channel-setting').css('color','#fff');
+            $button.removeAttr('disabled');
         });
-
     });
+
 });
